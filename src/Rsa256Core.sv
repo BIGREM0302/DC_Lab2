055622f56284ef623e609dd3f771351bb27a3f51
MaxRem <maxzheng0302@gmail.com>
2025-03-20 14:10:11 +0800

    Basic structure

--- src/Rsa256Core.sv
+++ src/Rsa256Core.sv
@@ -1,3 +1,23 @@
+module RsaMount(
+	input 			i_clk,
+	input			i_rst,
+	input	[255:0] y,
+	input	[255:0] N,
+);
+//deal with y*2^256 mod(N)
+
+endmodule
+
+module RsaPrep(
+	input			i_clk,
+	input			i_rst,
+	input			t_r,
+	output			t_w,
+);
+//use Montgomery Algorithm
+
+endmodule
+
 module Rsa256Core (
 	input          i_clk,
 	input          i_rst,
@@ -11,5 +31,52 @@ module Rsa256Core (
 
 // operations for RSA256 decryption
 // namely, the Montgomery algorithm
+parameter IDLE = 2'd0, PREP = 2'd1, MONT = 2'd2, CALC = 2'd3;
+
+logic[1:0] state_w, state_r;
+logic[7:0] counter_w, counter_r;
+logic rsa_prep_done, rsa_mont_done;
+logic [255:0] rsa_mont_out;
+
+always_comb begin
+	state_w = state_r;
+	case(state_r)
+		IDLE:begin
+			if(i_start) begin
+				state_w = PREP;
+			end
+		end
+		PREP:begin
+			if(rsa_prep_done) begin
+				state_w = MONT;
+			end
+		end
+		MONT:begin
+			if(rsa_mont_done) begin
+				state_w = CALC;
+			end
+		end
+		CALC:begin
+			if(counter_r != 8'd255) begin
+				state_w = MONT;
+			end
+			else begin
+				state_w = IDLE;
+			end
+		end
+	endcase
+end
+
+always_ff@(posedge i_clk or posedge i_rst) begin
+	if(i_rst)begin
+		//reset condition
+		state_r <= IDLE;
+		counter_r <= 0;
+	end
+	else begin
+		state_r <= state_w;
+		counter_r <= counter_w;
+	end
+end
 
 endmodule
